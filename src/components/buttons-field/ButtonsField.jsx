7757d8ce675e9ab22432e03dc16e91ca69691405
Gleb <basketking57@gmail.com>
2022-12-05 08:16:12 +0200

    create calculator logic with  math js

--- src/components/buttons-field/ButtonsField.jsx
+++ src/components/buttons-field/ButtonsField.jsx
@@ -2,7 +2,7 @@ import CalculatorButton from '../calculator-button/CalculatorButton'
 
 import './ButtonsField.scss';
 
-const ButtonsField = () => {
+const ButtonsField = ({ addDigit, addOperator, clearDisplay, addPoint, calculateResult, clearLastInputValue, addPercent }) => {
 
     return (
         <div className="buttons__field">
@@ -10,72 +10,91 @@ const ButtonsField = () => {
                 <CalculatorButton eachButton={'key__c'}
                     buttonColor={'key__dark'}
                     keyContent={'C'}
+                    handleacAction={() => clearDisplay()}
                 />
                 <CalculatorButton eachButton={'key__ce'}
                     buttonColor={'key__dark'}
                     keyContent={'CE'}
+                    handleacAction={() => clearLastInputValue()}
                 />
                 <CalculatorButton eachButton={'key__persent'}
                     buttonColor={'key__dark'}
                     keyContent={'%'}
+                    handleacAction={() => addPercent()}
                 />
             </div>
             <div className="center__side">
                 <CalculatorButton eachButton={'key__7'}
                     keyContent={'7'}
+                    handleacAction={() => addDigit(7)}
                 />
                 <CalculatorButton eachButton={'key__8'}
                     keyContent={'8'}
+                    handleacAction={() => addDigit(8)}
                 />
                 <CalculatorButton eachButton={'key__9'}
                     keyContent={'9'}
+                    handleacAction={() => addDigit(9)}
                 />
                 <CalculatorButton eachButton={'key__4'}
                     keyContent={'4'}
+                    handleacAction={() => addDigit(4)}
                 />
                 <CalculatorButton eachButton={'key__5'}
                     keyContent={'5'}
+                    handleacAction={() => addDigit(5)}
                 />
                 <CalculatorButton eachButton={'key__6'}
                     keyContent={'6'}
+                    handleacAction={() => addDigit(6)}
                 />
                 <CalculatorButton eachButton={'key__1'}
                     keyContent={'1'}
+                    handleacAction={() => addDigit(1)}
                 />
                 <CalculatorButton eachButton={'key__2'}
                     keyContent={'2'}
+                    handleacAction={() => addDigit(2)}
                 />
                 <CalculatorButton eachButton={'key__3'}
                     keyContent={'3'}
+                    handleacAction={() => addDigit(3)}
                 />
                 <CalculatorButton eachButton={'key__0'}
                     keyContent={'0'}
+                    handleacAction={() => addDigit(0)}
                 />
                 <CalculatorButton eachButton={'key__point'}
                     buttonColor={'key__yellow'}
                     keyContent={'.'}
+                    handleacAction={() => addPoint()}
                 />
             </div>
             <div className="right__side">
                 <CalculatorButton eachButton={'key__divide'}
                     buttonColor={'key__dark'}
                     keyContent={'÷'}
+                    handleacAction={() => addOperator('÷')}
                 />
                 <CalculatorButton eachButton={'key__multiply'}
                     buttonColor={'key__dark'}
                     keyContent={'x'}
+                    handleacAction={() => addOperator('x')}
                 />
                 <CalculatorButton eachButton={'key__minus'}
                     buttonColor={'key__dark'}
                     keyContent={'-'}
+                    handleacAction={() => addOperator('-')}
                 />
                 <CalculatorButton eachButton={'key__equals'}
                     buttonColor={'key__yellow'}
                     keyContent={'='}
+                    handleacAction={() => calculateResult()}
                 />
                 <CalculatorButton eachButton={'key__plus'}
                     buttonColor={'key__dark'}
                     keyContent={'+'}
+                    handleacAction={() => addOperator('+')}
                 />
             </div>
         </div>
--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,13 +1,100 @@
+import { useState } from 'react';
+import { math, limitedEvaluate, formatOptions } from '../math-config/mathConfig';
+
 import CalculatorDisplay from '../calculator-display/CalculatorDisplay';
 import ButtonsField from '../buttons-field/ButtonsField';
+
 import './calculator.scss';
 
 const Calculator = () => {
-    const displayValue = '0'
+
+    const [displayValue, setDisplayValue] = useState('0');
+
+    const addDigit = (digit) => {
+        setDisplayValue(displayValue => displayValue === '0' ? math.format(digit) : displayValue + math.format(digit));
+    }
+
+    const clearDisplay = () => {
+        setDisplayValue(displayValue => displayValue.length === 1 ? '' : displayValue = '');
+    }
+    const clearLastInputValue = () => {
+        setDisplayValue(displayValue => displayValue.length === 1 ? '' : displayValue.slice(0, -1));
+    }
+
+    const addOperator = (operator) => {
+        if (operator === '-') {
+            setDisplayValue(displayValue => displayValue.substring(-1) === '.' ? displayValue : displayValue + operator);
+        } else {
+            // eslint-disable-next-line
+            setDisplayValue(displayValue => /[\+\-x÷\.]$/.test(displayValue) ? displayValue : displayValue + operator);
+        }
+    }
+
+
+    const addPoint = () => {
+        // eslint-disable-next-line
+        if (/[\+\-x÷]$/.test(displayValue)) {
+            return setDisplayValue(displayValue => displayValue + '0.');
+            // eslint-disable-next-line
+        } else if (/[\+\-x÷]/.test(displayValue.slice(displayValue.lastIndexOf('.')))) {
+            return setDisplayValue(displayValue => displayValue + '.');
+        } else if (displayValue.length < 1) {
+            return setDisplayValue(displayValue => displayValue + '0.');
+        } else if (displayValue.lastIndexOf('.') === -1) {
+            return setDisplayValue(displayValue => displayValue + '.');
+        } else {
+            return;
+        }
+    }
+
+    const addPercent = () => {
+        // eslint-disable-next-line
+        if (/[\+\-x÷]$/.test(displayValue)) {
+            return setDisplayValue(displayValue => displayValue + '0%');
+        } else if (displayValue.lastIndexOf('%') === -1) {
+            return setDisplayValue(displayValue => displayValue + '%');
+            // eslint-disable-next-line
+        } else if (/[\+\-x÷]/.test(displayValue.slice(displayValue.lastIndexOf('%')))) {
+            return setDisplayValue(displayValue => displayValue + '%');
+        } else {
+            return;
+        }
+    }
+
+    const calculateResult = () => {
+        let result;
+
+        let convertOperators = displayValue.replace(/x/g, '*').replace(/÷/g, '/');
+
+        if (displayValue.substring(-1) === '.') {
+            return;
+        }
+
+        try {
+            result = math.format(limitedEvaluate(convertOperators), formatOptions).replace(/\.0+$|0+$/, '');
+        } catch (error) {
+            return;
+        }
+
+        if (result === 'Infinity' || result === '-Infinity' || result === 'NaN') {
+            return;
+        } else {
+            setDisplayValue(displayValue => displayValue = result);
+        }
+    }
+
     return (
         <div className="main">
             <CalculatorDisplay displayValue={displayValue} />
-            <ButtonsField />
+            <ButtonsField
+                addDigit={addDigit}
+                addOperator={addOperator}
+                clearDisplay={clearDisplay}
+                addPoint={addPoint}
+                calculateResult={calculateResult}
+                clearLastInputValue={clearLastInputValue}
+                addPercent={addPercent}
+            />
         </div>
     )
 }
